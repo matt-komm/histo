7abd3b72a1dac18c6d6f04b41955268bb66969b8
daiktas <vilius.cepaitis@gmail.com>
2021-05-09 22:30:24 +0100

    rm

--- plotting/plotter.py
+++ plotting/plotter.py
@@ -7,7 +7,7 @@ import numpy as np
 from array import array
 import argparse
 
-from histo import Process, Variable, Sample, lumi
+from histo import Process, Variable, Sample
 signal_xsec = 1.
 
 parser = argparse.ArgumentParser()
@@ -17,9 +17,9 @@ parser.add_argument("-r", "--region", action="store", dest="region")
 parser.add_argument("-y", "--year", action="store", dest="year", default="2016")
 parser.add_argument("--region_file", action="store", dest="region_file", default="regions.json")
 parser.add_argument("--variable_file", action="store", dest="variable_file", default="variables.json")
-parser.add_argument("--samples_file", action="store", dest="samples_file", default="samples.yml")
-parser.add_argument("--ntuple_path", action="store", dest="ntuple_path", default="/vols/cms/vc1117/LLP/nanoAOD_friends/HNL/15Mar21_2l")
-parser.add_argument("--output_dir", action="store", dest="output_dir", default="plots")
+parser.add_argument("--samples_file", action="store", dest="samples_file", default="config/samples.yml")
+parser.add_argument("--ntuple_path", action="store", dest="ntuple_path", default="/vols/cms/vc1117/LLP/nanoAOD_friends/HNL/20Apr21_2l_notagger/")
+parser.add_argument("--output_dir", action="store", dest="output_dir", default="tagger_efficiency/plots_tagger_eff")
 parser.add_argument("--test", action="store_true", dest="one_file", default=False)
 
 #parser.add_argument("-d", "--data", action="store", dest="data_type")
@@ -71,19 +71,23 @@ text_dict = {
              "vgamma": "V#gamma*",
              "topbkg": "t#bar{t}",
              "qcd": "Multijet",
-             #"muon": "muon",
-             #"electron": "electron",
-             #"data": "data",
-             "HNL_majorana_all_ctau1p0e01_massHNL10p0_Vall3p721e-04": "HNL",
+             "muon": "muon",
+             "electron": "electron",
+             "data": "Data",
              }
+
+if "SR" in category:
+    text_dict["HNL_majorana_all_ctau1p0e02_massHNL4p5_Vall1p016e-03"] = "m_{N} = 4.5 GeV, c#tau_{0} = 10 cm, V_{e}=V_{#mu}"
+    text_dict["HNL_majorana_all_ctau1p0e00_massHNL10p0_Vall1p177e-03"] = "m_{N} = 10 GeV, c#tau_{0} = 1 mm, V_{e}=V_{#mu}"
+    text_dict.pop("data")
+    text_dict.pop("muon")
+    text_dict.pop("electron")
+
 
 for process_name in text_dict.keys():
     if process_name not in samples_dict.keys():
         continue
     process = samples_dict[process_name]
-#for process_name, process in samples_dict.items():
-    #if process_name not in text_dict.keys():
-        #continue
     isMC=True
     if "muon" in process_name or "electron" in process_name:
         if process_name!=data_type:
@@ -94,7 +98,7 @@ for process_name in text_dict.keys():
 
     if "HNL" in process_name:
         process = Process("HNL", text_dict[process_name], linecolor='#bd0000')
-        process.add(Sample(process_name, ntuple_path, ["{}-{}".format(process_name, year)], year=year, limits=False))
+        process.Add(Sample(process_name, ntuple_path, ["{}-{}".format(process_name, year)], year=year, limits=False))
 
     else:
         subprocesses = process[int(year)]
@@ -102,7 +106,7 @@ for process_name in text_dict.keys():
         for sample_name, sample_list in subprocesses.items():
             print(sample_name)
             sample = Sample(sample_name, ntuple_path, sample_list, isMC=isMC, year=year, oneFile=args.one_file, cut=weight)
-            process.add(sample)
+            process.Add(sample)
         
     process.Define(varname.replace("-", "m").replace("(","_").replace(")","_").replace("/", "over"), variable_infos[0])
     processes.append(process)
@@ -113,8 +117,8 @@ for process in processes:
     print(process.name)
     if "HNL" in process.name:
         hist = process.Histo1D(variable.args, varname.replace("-", "m").replace("(","_").replace(")","_").replace("/", "over"), weight="weightNominalHNL_7")
-        hist.Scale(1000.)
-        variable.Add(hist, "HNL, V_{e}=V_{#mu} (x1000)", isSignal=True, isData=False)
+        #hist.Scale(1000.)
+        variable.Add(hist, process.title, isSignal=True, isData=False)
     else:
         if process.name == "data":
             isData = True
